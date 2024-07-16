d7e887f87b75782c93ff1f367432f230dc4835a1
Muhammad Hammad <muhammad.hammad@din.global>
2024-07-17 02:18:17 +0500

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,11 +7,11 @@ const settings = {
 
 //SEO Related settings
 const seo = {
-  title: "Ashutosh's Portfolio",
+  title: "Hammad's Portfolio",
   description:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   og: {
-    title: "Ashutosh Hathidara Portfolio",
+    title: "Muhammad Hammad Portfolio",
     type: "website",
     url: "http://ashutoshhathidara.com/",
   },
@@ -19,7 +19,7 @@ const seo = {
 
 //Home Page
 const greeting = {
-  title: "Ashutosh Hathidara",
+  title: "Muhammad Hammad",
   logo_name: "AshutoshHathidara",
   nickname: "layman_brother",
   subTitle:
